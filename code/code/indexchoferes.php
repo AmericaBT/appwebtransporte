07f21a57c9189154df25b69e4c8dc581e1854292
AmericaBT <america.perez@uttn.mx>
2025-11-04 03:03:59 -0600

    Cambio para cargar los css

--- code/code/indexchoferes.php
+++ code/code/indexchoferes.php
@@ -27,8 +27,8 @@ if (!$resultado) {
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta charset="utf-8" />
-    <link rel="stylesheet" href="/css/styleglobals.css" />
-    <link rel="stylesheet" href="/css/stylechoferes.css" />
+    <link rel="stylesheet" href="/css/styleglobals.css?v=<?php echo time(); ?>" />
+    <link rel="stylesheet" href="/css/stylechoferes.css?v=<?php echo time(); ?>" />
     <title>Alumnos</title>
 </head>
 <body>
