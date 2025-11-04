15848faf38bf6a55474d86b9e873b72b4d6270e9
AmericaBT <america.perez@uttn.mx>
2025-11-04 03:18:24 -0600

    Cambio para cargar los css

--- code/code/choferes_registros.php
+++ code/code/choferes_registros.php
@@ -18,8 +18,8 @@ if (!$conexion) {
   <head>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta charset="utf-8" />
-    <link rel="stylesheet" href="/css/styleglobals.css" />
-    <link rel="stylesheet" href="/css/styleregistros.css" />
+    <link rel="stylesheet" href="/css/styleglobals.css?v=<?php echo time(); ?>" />
+    <link rel="stylesheet" href="/css/styleregistros.css?v=<?php echo time(); ?>" />
   </head>
   <body>
     <div class="choferes-registros">
